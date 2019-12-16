a5c45668f5d6704d90f93aa1ab1e521520ed69c1
Iuliia <romanova.works@yandex.ru>
2019-12-16 21:10:31 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+let someStr = 'Изменил, сохранил, собрал';
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2); 
+
+console.log(doubledNumbers); 
